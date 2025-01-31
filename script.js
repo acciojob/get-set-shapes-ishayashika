fb23ea864263214617dd3fa32069aaa1bf991269
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-01-31 18:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //complete this code
 class Rectangle {
 	constructor(width,height){
-		this.width=width;
-		this.height=height;
+		this._width=width;
+		this._height=height;
 	}
 	get width(){
-		return this.width;
+		return this._width;
 	}
 	get height(){
-		return this.height;
+		return this._height;
 	}
 	getArea(){
-		return this.width*this.height;
+		return this._width*this._height;
 	}
 }
 
@@ -20,17 +20,17 @@ class Square extends Rectangle {
 		super(side,side);
 	}
 	getPerimeter(){
-		return 4*this.width;
+		return this._side*4;
 	}
 }
 const rectangle=new Rectangle(5,10);
-console.log(rectangle.width);
-console.log(rectangle.height);
+console.log(rectangle._width);
+console.log(rectangle._height);
 console.log(rectangle.getArea());
 
 const square=new Square(7);
-console.log(square.width);
-console.log(square.height);
+console.log(square._side);
+console.log(square._side);
 console.log(square.getArea());
 console.log(square.getPerimeter());
 
